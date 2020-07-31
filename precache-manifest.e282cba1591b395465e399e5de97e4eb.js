3a5036c7df162154fde14e344e035697b3b7a99e
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-07-31 19:32:57 +0100

    Updates

---
+++ precache-manifest.e282cba1591b395465e399e5de97e4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0a3b3a928dfec2bad1e5025e32a458",
+    "url": "/Testing-React-Redux-with-Jest-and-Enzyme/index.html"
+  },
+  {
+    "revision": "2daad6d49c56a3214998",
+    "url": "/Testing-React-Redux-with-Jest-and-Enzyme/static/css/main.1db451a6.chunk.css"
+  },
+  {
+    "revision": "0b2b879f7b99d91257d8",
+    "url": "/Testing-React-Redux-with-Jest-and-Enzyme/static/js/2.490fefbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Testing-React-Redux-with-Jest-and-Enzyme/static/js/2.490fefbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2daad6d49c56a3214998",
+    "url": "/Testing-React-Redux-with-Jest-and-Enzyme/static/js/main.43c1663e.chunk.js"
+  },
+  {
+    "revision": "cd55c103d87aaed37675",
+    "url": "/Testing-React-Redux-with-Jest-and-Enzyme/static/js/runtime-main.ac7a2b92.js"
+  },
+  {
+    "revision": "123ba1a14f911aa436a3921928a99990",
+    "url": "/Testing-React-Redux-with-Jest-and-Enzyme/static/media/logo.123ba1a1.png"
+  }
+]);
--- src/createStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
-
-export const middlewares = [ReduxThunk];
-
-export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
-
-export const store = createStoreWithMiddleware(reducers);
