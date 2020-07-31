f26b55d87b2e09692add0532e88ca7cacd88360a
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-07-31 22:21:39 +0100

    Testing script

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/header';
 import Headline from './components/headline';
+
 import './App.scss';
 
 const tempArr = [{
@@ -12,15 +13,19 @@ const tempArr = [{
   onlineStatus: true
 }]
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <section className="main">
-        <Headline header="Post" desc="Click the button to render posts" tempArr={tempArr} />
-      </section>
-    </div>
-  );
+class App extends React.Component {
+    
+    render() {
+        return (
+            <div className="App">
+              <Header />
+              <section className="main">
+                <Headline header="Post" desc="Click the button to render posts" tempArr={tempArr} />
+              </section>
+            </div>
+          );
+    }
 }
+
 
 export default App;
