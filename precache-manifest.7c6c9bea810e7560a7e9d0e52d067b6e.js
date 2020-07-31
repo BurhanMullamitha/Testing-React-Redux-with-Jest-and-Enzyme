86a1d7e978af0621204e04e3b18e167ba9b226c4
BurhanMullamitha <42492054+BurhanMullamitha@users.noreply.github.com>
2020-07-31 18:06:50 +0000

    Deploying to gh-pages from  @ 91e0b884796a1c74c29fa730c601bd24685bc510 🚀

---
+++ precache-manifest.7c6c9bea810e7560a7e9d0e52d067b6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f720cf8ce4ab7433370174138e3bcceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa0fe43958d0aca3e91",
+    "url": "/static/css/main.1db451a6.chunk.css"
+  },
+  {
+    "revision": "0b2b879f7b99d91257d8",
+    "url": "/static/js/2.490fefbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.490fefbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa0fe43958d0aca3e91",
+    "url": "/static/js/main.252b96d9.chunk.js"
+  },
+  {
+    "revision": "9ec6419446117bc0c77b",
+    "url": "/static/js/runtime-main.be3a8572.js"
+  },
+  {
+    "revision": "123ba1a14f911aa436a3921928a99990",
+    "url": "/static/media/logo.123ba1a1.png"
+  }
+]);
